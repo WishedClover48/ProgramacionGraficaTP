47b1406ba2aacf100bc39f48ecfebb50d3fa1ec3
EgiglioUade <146669271+Zeki19@users.noreply.github.com>
2025-06-23 23:22:24 -0300

    Added PostProcess cs

---
+++ Assets/Shaders/PostProcess/PP.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+public class PP : MonoBehaviour
+{
+    public Shader shader;
+    private Material _mat;
+    public float intensity;
+
+    private void Start()
+    {
+        _mat = new Material(shader);
+    }
+
+    private void Update()
+    {
+        _mat.SetFloat("_Intensity", intensity);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _mat);
+    }
+}
